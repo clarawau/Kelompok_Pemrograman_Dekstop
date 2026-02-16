bee2ec538e25be4f3e7c5641ef69d9625b6e5e94
clarawau <143706022+clarawau@users.noreply.github.com>
2026-02-17 00:29:44 +0700

    coba

--- tugas1_progdesk/Form2.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form4
-	Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-
-	End Sub
-
-	Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-	End Sub
-End Class
