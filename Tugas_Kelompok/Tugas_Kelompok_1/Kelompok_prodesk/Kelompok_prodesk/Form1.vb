78fd5c25a50f5540a7750254f7d8ddeed892e690
clarawau <143706022+clarawau@users.noreply.github.com>
2026-02-17 00:24:42 +0700

    coba2

---
+++ Tugas_Kelompok/Tugas_Kelompok_1/Kelompok_prodesk/Kelompok_prodesk/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+	End Sub
+
+	Private Sub Btnplay_Click(sender As Object, e As EventArgs) Handles Btnplay.Click
+		Form3.Show()
+		Me.Hide()
+	End Sub
+
+	Private Sub Pcbmenu_Click(sender As Object, e As EventArgs) Handles Pcbmenu.Click
+
+	End Sub
+End Class
---
+++ Tugas_Kelompok/Tugas_Kelompok_1/Kelompok_prodesk/Kelompok_prodesk/Form2.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form4
+	Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+	End Sub
+
+	Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+	End Sub
+End Class
