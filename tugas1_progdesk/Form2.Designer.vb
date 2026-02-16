00c6f1a6c083fa8e3f28ec663c568ee080126853
clarawau <143706022+clarawau@users.noreply.github.com>
2026-02-17 00:30:03 +0700

    cobalagi

---
+++ tugas1_progdesk/Form2.Designer.vb
@@ -0,0 +1,384 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form4
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox12 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox13 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox14 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox15 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox16 = New System.Windows.Forms.PictureBox()
+		Me.PictureBox17 = New System.Windows.Forms.PictureBox()
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox16, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.PictureBox17, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Ravie", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.Location = New System.Drawing.Point(38, 30)
+		Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(190, 43)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "SCORE :"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Font = New System.Drawing.Font("Ravie", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.Location = New System.Drawing.Point(448, 30)
+		Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(165, 43)
+		Me.Label2.TabIndex = 1
+		Me.Label2.Text = "TIME :"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Font = New System.Drawing.Font("Ravie", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label3.Location = New System.Drawing.Point(874, 30)
+		Me.Label3.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(303, 43)
+		Me.Label3.TabIndex = 2
+		Me.Label3.Text = "BONUS point:"
+		'
+		'Button1
+		'
+		Me.Button1.BackColor = System.Drawing.Color.WhiteSmoke
+		Me.Button1.ForeColor = System.Drawing.Color.Brown
+		Me.Button1.Location = New System.Drawing.Point(180, 539)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(190, 116)
+		Me.Button1.TabIndex = 4
+		Me.Button1.Text = "RETRY"
+		Me.Button1.UseVisualStyleBackColor = False
+		'
+		'PictureBox2
+		'
+		Me.PictureBox2.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox2.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox2.Location = New System.Drawing.Point(566, 139)
+		Me.PictureBox2.Name = "PictureBox2"
+		Me.PictureBox2.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox2.TabIndex = 5
+		Me.PictureBox2.TabStop = False
+		Me.PictureBox2.Visible = False
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_a_baby_panda_with_big_belly_watching_sitting_on_floor_png_image_15422516
+		Me.PictureBox1.Location = New System.Drawing.Point(12, 139)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(499, 586)
+		Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox1.TabIndex = 3
+		Me.PictureBox1.TabStop = False
+		'
+		'PictureBox3
+		'
+		Me.PictureBox3.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox3.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox3.Location = New System.Drawing.Point(782, 139)
+		Me.PictureBox3.Name = "PictureBox3"
+		Me.PictureBox3.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox3.TabIndex = 6
+		Me.PictureBox3.TabStop = False
+		Me.PictureBox3.Visible = False
+		'
+		'PictureBox4
+		'
+		Me.PictureBox4.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox4.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox4.Location = New System.Drawing.Point(994, 139)
+		Me.PictureBox4.Name = "PictureBox4"
+		Me.PictureBox4.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox4.TabIndex = 7
+		Me.PictureBox4.TabStop = False
+		Me.PictureBox4.Visible = False
+		'
+		'PictureBox5
+		'
+		Me.PictureBox5.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox5.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox5.Location = New System.Drawing.Point(1207, 139)
+		Me.PictureBox5.Name = "PictureBox5"
+		Me.PictureBox5.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox5.TabIndex = 8
+		Me.PictureBox5.TabStop = False
+		Me.PictureBox5.Visible = False
+		'
+		'PictureBox6
+		'
+		Me.PictureBox6.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox6.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox6.Location = New System.Drawing.Point(566, 293)
+		Me.PictureBox6.Name = "PictureBox6"
+		Me.PictureBox6.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox6.TabIndex = 9
+		Me.PictureBox6.TabStop = False
+		Me.PictureBox6.Visible = False
+		'
+		'PictureBox7
+		'
+		Me.PictureBox7.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox7.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox7.Location = New System.Drawing.Point(782, 293)
+		Me.PictureBox7.Name = "PictureBox7"
+		Me.PictureBox7.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox7.TabIndex = 10
+		Me.PictureBox7.TabStop = False
+		Me.PictureBox7.Visible = False
+		'
+		'PictureBox8
+		'
+		Me.PictureBox8.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox8.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox8.Location = New System.Drawing.Point(994, 293)
+		Me.PictureBox8.Name = "PictureBox8"
+		Me.PictureBox8.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox8.TabIndex = 11
+		Me.PictureBox8.TabStop = False
+		Me.PictureBox8.Visible = False
+		'
+		'PictureBox9
+		'
+		Me.PictureBox9.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox9.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox9.Location = New System.Drawing.Point(1207, 293)
+		Me.PictureBox9.Name = "PictureBox9"
+		Me.PictureBox9.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox9.TabIndex = 12
+		Me.PictureBox9.TabStop = False
+		Me.PictureBox9.Visible = False
+		'
+		'PictureBox10
+		'
+		Me.PictureBox10.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox10.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox10.Location = New System.Drawing.Point(566, 449)
+		Me.PictureBox10.Name = "PictureBox10"
+		Me.PictureBox10.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox10.TabIndex = 13
+		Me.PictureBox10.TabStop = False
+		Me.PictureBox10.Visible = False
+		'
+		'PictureBox11
+		'
+		Me.PictureBox11.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox11.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox11.Location = New System.Drawing.Point(782, 449)
+		Me.PictureBox11.Name = "PictureBox11"
+		Me.PictureBox11.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox11.TabIndex = 14
+		Me.PictureBox11.TabStop = False
+		Me.PictureBox11.Visible = False
+		'
+		'PictureBox12
+		'
+		Me.PictureBox12.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox12.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox12.Location = New System.Drawing.Point(994, 449)
+		Me.PictureBox12.Name = "PictureBox12"
+		Me.PictureBox12.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox12.TabIndex = 15
+		Me.PictureBox12.TabStop = False
+		Me.PictureBox12.Visible = False
+		'
+		'PictureBox13
+		'
+		Me.PictureBox13.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox13.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox13.Location = New System.Drawing.Point(1207, 449)
+		Me.PictureBox13.Name = "PictureBox13"
+		Me.PictureBox13.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox13.TabIndex = 16
+		Me.PictureBox13.TabStop = False
+		Me.PictureBox13.Visible = False
+		'
+		'PictureBox14
+		'
+		Me.PictureBox14.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox14.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox14.Location = New System.Drawing.Point(566, 598)
+		Me.PictureBox14.Name = "PictureBox14"
+		Me.PictureBox14.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox14.TabIndex = 17
+		Me.PictureBox14.TabStop = False
+		Me.PictureBox14.Visible = False
+		'
+		'PictureBox15
+		'
+		Me.PictureBox15.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox15.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox15.Location = New System.Drawing.Point(782, 598)
+		Me.PictureBox15.Name = "PictureBox15"
+		Me.PictureBox15.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox15.TabIndex = 18
+		Me.PictureBox15.TabStop = False
+		Me.PictureBox15.Visible = False
+		'
+		'PictureBox16
+		'
+		Me.PictureBox16.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox16.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox16.Location = New System.Drawing.Point(994, 598)
+		Me.PictureBox16.Name = "PictureBox16"
+		Me.PictureBox16.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox16.TabIndex = 19
+		Me.PictureBox16.TabStop = False
+		Me.PictureBox16.Visible = False
+		'
+		'PictureBox17
+		'
+		Me.PictureBox17.BackColor = System.Drawing.SystemColors.ButtonShadow
+		Me.PictureBox17.Image = Global.Kelompok_prodesk.My.Resources.Resources.pngtree_cartoon_character_of_a_question_mark_symbolizing_curiosity_and_uncertainty_png_image_13290637
+		Me.PictureBox17.Location = New System.Drawing.Point(1207, 598)
+		Me.PictureBox17.Name = "PictureBox17"
+		Me.PictureBox17.Size = New System.Drawing.Size(186, 127)
+		Me.PictureBox17.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox17.TabIndex = 20
+		Me.PictureBox17.TabStop = False
+		Me.PictureBox17.Visible = False
+		'
+		'Form4
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(22.0!, 42.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(2671, 1265)
+		Me.Controls.Add(Me.PictureBox17)
+		Me.Controls.Add(Me.PictureBox16)
+		Me.Controls.Add(Me.PictureBox15)
+		Me.Controls.Add(Me.PictureBox14)
+		Me.Controls.Add(Me.PictureBox13)
+		Me.Controls.Add(Me.PictureBox12)
+		Me.Controls.Add(Me.PictureBox11)
+		Me.Controls.Add(Me.PictureBox10)
+		Me.Controls.Add(Me.PictureBox9)
+		Me.Controls.Add(Me.PictureBox8)
+		Me.Controls.Add(Me.PictureBox7)
+		Me.Controls.Add(Me.PictureBox6)
+		Me.Controls.Add(Me.PictureBox5)
+		Me.Controls.Add(Me.PictureBox4)
+		Me.Controls.Add(Me.PictureBox3)
+		Me.Controls.Add(Me.PictureBox2)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.PictureBox1)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
+		Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+		Me.Name = "Form4"
+		Me.Text = "Form2"
+		CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox16, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.PictureBox17, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label2 As Label
+	Friend WithEvents Label3 As Label
+	Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents Button1 As Button
+	Friend WithEvents PictureBox2 As PictureBox
+	Friend WithEvents PictureBox3 As PictureBox
+	Friend WithEvents PictureBox4 As PictureBox
+	Friend WithEvents PictureBox5 As PictureBox
+	Friend WithEvents PictureBox6 As PictureBox
+	Friend WithEvents PictureBox7 As PictureBox
+	Friend WithEvents PictureBox8 As PictureBox
+	Friend WithEvents PictureBox9 As PictureBox
+	Friend WithEvents PictureBox10 As PictureBox
+	Friend WithEvents PictureBox11 As PictureBox
+	Friend WithEvents PictureBox12 As PictureBox
+	Friend WithEvents PictureBox13 As PictureBox
+	Friend WithEvents PictureBox14 As PictureBox
+	Friend WithEvents PictureBox15 As PictureBox
+	Friend WithEvents PictureBox16 As PictureBox
+	Friend WithEvents PictureBox17 As PictureBox
+End Class
